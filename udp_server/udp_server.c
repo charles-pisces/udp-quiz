f34d37f429cb5b3b6ac1bbcd41413ede92ac07c3
Charles_Chen <charles.pisces@gmail.com>
2023-01-18 14:44:30 +0800

    [UDP_Server] Code refactor and add echo function.

--- udp_server/udp_server.c
+++ udp_server/udp_server.c
@@ -7,8 +7,9 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define SERVER_PORT     12345
-#define BUF_SIZE        255
+#define SERVER_PORT             12345
+#define BUF_SIZE                255
+
 #define CHECK_FUNC_ERR(func, rc) {									\
 			rc = func; 											\
 			if(rc < 0) { 											\
@@ -37,7 +38,9 @@ static int set_sock_nonblock(int sock)
 }
 
 
-static int bind_udp_port(int sock, int port) {
+static int udp_bind_port(int sock, 
+                         int port) 
+{
     int rc = -1;
     struct sockaddr_in server;
     
@@ -57,21 +60,21 @@ static int bind_udp_port(int sock, int port) {
 		return rc;
 	}
 
-    printf("Bind successfully.\n");
     return rc;
 }
 
 
-static int udp_recv(int sock, char* buf)
+static int udp_recv(int sock, 
+                    char* buf, 
+                    struct sockaddr_in* from)
 {
     int rc = -1;
-    struct sockaddr_in from;
     socklen_t sock_len = sizeof(struct sockaddr_in);
 
     memset(buf, 0, BUF_SIZE);  
-    rc = recvfrom(sock, buf, BUF_SIZE, 0, (struct sockaddr*)&from, &sock_len);
+    rc = recvfrom(sock, buf, BUF_SIZE, 0, (struct sockaddr *)from, &sock_len);
     if (rc > 0) {
-        printf("The message received from %s  Data is :%s\n", inet_ntoa(from.sin_addr), buf);
+        printf("Received : %s\n", buf);
     } else {
         if (errno == EWOULDBLOCK || errno == EAGAIN) {
             rc = 0;
@@ -86,30 +89,41 @@ static int udp_recv(int sock, char* buf)
 }
 
 
+static int udp_send(int sock, 
+                    char* buf, 
+                    struct sockaddr_in *to)
+{
+    int rc = -1;
+    
+    rc = sendto(sock, buf, BUF_SIZE, 0, (struct sockaddr *)to, sizeof(struct sockaddr_in));
+    if (rc == -1)
+        perror("sendto call failed\n");
+        
+    return rc;
+}
+
+
 static int udp_select_check(int sock) 
 {	
     struct timeval timeout; 
     fd_set r_fd;
     
     timeout.tv_sec = 0; 
-    timeout.tv_usec = 500; 
+    timeout.tv_usec = 50; 
     FD_ZERO(&r_fd); 
     FD_SET(sock, &r_fd); 
     
     int rc = select(sock + 1, &r_fd, NULL, NULL, &timeout); 
-    if (rc < 0) {
+    if (rc < 0)
         printf("select error! socket : %d, rc : %d, errno : %d\n", sock, rc, errno);
-        close(sock);
-    }
         
     // timeout
     if (rc == 0) {
 
     }
 
-    if (FD_ISSET(sock, &r_fd)) {  
+    if (FD_ISSET(sock, &r_fd))
         rc = 1;
-    }
 
     return rc;
 }
@@ -118,26 +132,25 @@ static int udp_select_check(int sock)
 int main (int argc, char *argv[])
 {
     int rc = -1;
-    char buf[BUF_SIZE];
     int sock = -1;
+    char buf[BUF_SIZE];
+    struct sockaddr_in from;
+    socklen_t sock_len = sizeof(struct sockaddr_in);
+    
+    printf("UDP Server Start.\n");
 
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock == -1) {
-       perror("socket create fail!\n");
-       exit(-1);
-    }
-    printf("socket fd = %d\n", sock);
-
-    CHECK_FUNC_ERR(bind_udp_port(sock, SERVER_PORT), rc)
-        
+    CHECK_FUNC_ERR(socket(AF_INET, SOCK_DGRAM, 0), sock)
+    CHECK_FUNC_ERR(udp_bind_port(sock, SERVER_PORT), rc)
+    
     while (true) {
         CHECK_FUNC_ERR(udp_select_check(sock), rc)
-        CHECK_FUNC_ERR(udp_recv(sock, buf), rc)
-     }
+        CHECK_FUNC_ERR(udp_recv(sock, buf, &from), rc)
+        if (rc > 0)
+            CHECK_FUNC_ERR(udp_send(sock, buf, &from), rc)
+    }
 
-     close(sock);
-     return 0;
-
+    close(sock);
+    return 0;
 ERR:
     close(sock);
     return -1;
