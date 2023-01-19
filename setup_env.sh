fb7af91b551d592b81db2ceaee34fb0fcf85cb82
Charles_Chen <charles.pisces@gmail.com>
2023-01-19 12:18:38 +0800

    Remove old container before rebuild.

--- setup_env.sh
+++ setup_env.sh
@@ -4,6 +4,10 @@ sudo docker build -f Dockerfile -t debian-10-base .;
 # Config work space here
 workspace=/Users/charles/projects
 
+# Remove the old container first
+sudo docker rm -v -f udp_server;
+sudo docker rm -v -f udp_client;
+
 # Run udp server container in background
 sudo docker run -d -it --net host \
                 -v $workspace/udp-quiz/udp_server:/projects \
